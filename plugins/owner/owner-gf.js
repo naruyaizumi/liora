47886bc8509d8209f200a94e3b011c93a8372647
naruyaizumi <sexystyle088@gmil.com>
2025-09-21 17:40:40 +0000

    [!] AUTO PUSH: 🍧 Sinkronisasi otomatis berhasil dijalankan ~ Liora

--- plugins/owner/owner-gf.js
+++ plugins/owner/owner-gf.js
@@ -1,30 +1,33 @@
-import { join } from "path";
-import { readFileSync, existsSync } from "fs";
+import { join, extname } from "path"
+import { readFileSync, existsSync } from "fs"
 
 let handler = async (m, { conn, args, usedPrefix, command, __dirname }) => {
-    if (!args.length)
-        return m.reply(
-            `🍓 *Masukkan path file plugin yang ingin diambil~*\n\n*Contoh: ${usedPrefix + command} plugins owner owner-delsw*`
-        );
-    let target = join(...args);
-    if (!target.endsWith(".js")) target += ".js";
-    let filepath = join(__dirname, "../", target);
-    if (!existsSync(filepath)) return m.reply(`🍎 *File "${target}" tidak ditemukan!*`);
-    await conn.sendMessage(
-        m.chat,
-        {
-            document: readFileSync(filepath),
-            fileName: target.split("/").pop(),
-            mimetype: "text/javascript",
-            caption: `📂 *Berikut file: ${target}*\n🍡 *Semoga bermanfaat yaa~*`,
-        },
-        { quoted: m }
-    );
-};
+  if (!args.length)
+    return m.reply(
+      `🍓 *Masukkan path file yang ingin diambil~*\n\n*Contoh:* ${usedPrefix + command} plugins owner owner-delsw\n*Contoh:* ${usedPrefix + command} package.json`
+    )
 
-handler.help = ["getfile"];
-handler.tags = ["owner"];
-handler.command = /^(getfile|getplugin|gf)$/i;
-handler.mods = true;
+  let target = join(...args)
+  if (!extname(target)) target += ".js"
 
-export default handler;
+  let filepath = join(__dirname, "../", target)
+  if (!existsSync(filepath)) return m.reply(`🍎 *File "${target}" tidak ditemukan!*`)
+
+  await conn.sendMessage(
+    m.chat,
+    {
+      document: readFileSync(filepath),
+      fileName: target.split("/").pop(),
+      mimetype: "application/octet-stream",
+      caption: `📂 *Berikut file: ${target}* 🍡`,
+    },
+    { quoted: m }
+  )
+}
+
+handler.help = ["getfile <path>"]
+handler.tags = ["owner"]
+handler.command = /^(getfile|getplugin|gf)$/i
+handler.mods = true
+
+export default handler
